93b86f2c325c524be253352189e3e0fe0fc6d2f4
sina <sinasohrab84@gmail.com>
2023-08-31 15:13:21 +0330

    fixe project

--- csv_create/csv_regression.py
+++ csv_create/csv_regression.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-X=2*np.random.rand(100,1)
-y=4+3*X+np.random.rand(100,1)
-plt.scatter(X,y)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.rand(100, 1)
+plt.scatter(X, y)
 plt.xlabel("X")
 plt.ylabel("y")
 plt.show()
