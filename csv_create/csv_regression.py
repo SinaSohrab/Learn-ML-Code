a432c6347b42afdd02941c896491c03bb1fd060b
sina <sinasohrab84@gmail.com>
2023-09-10 17:12:06 +0330

    The project was updated

--- csv_create/csv_regression.py
+++ csv_create/csv_regression.py
@@ -1,9 +1,11 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
 
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.rand(100, 1)
-plt.scatter(X, y)
-plt.xlabel("X")
-plt.ylabel("y")
-plt.show()
+
+data = np.concatenate((X, y), axis=1)
+df = pd.DataFrame(data, columns=['X', 'y'])
+df.to_csv("csv_create/test2.csv", index=False)
+
